6466f726ca8185ed3c930871fe4d7b20736f3c54
Dave Jeffrey <mail@davidjeffrey.co.uk>
2016-06-14 22:19:15 +0100

    Basic serialization to string + test

--- src/graphqaml.ml
+++ src/graphqaml.ml
@@ -6,6 +6,21 @@ include Gqltypes
 let parse_query q =    
     try Ok (Parser.root_query Lexer.read (Lexing.from_string q)) with
     | SyntaxError msg ->
-        Error msg;
+        Error msg
     | Parser.Error ->
-        Error "Undefined Parser Error (sorry)\n";
+        Error "Undefined Parser Error (sorry)\n"
+
+let rec field_to_string field =
+    (match field.alias with
+    | Some alias -> alias ^ ":"
+    | None -> "") ^
+    field.identifier ^ 
+    (if List.length field.fields > 0 then 
+        " {" ^ ((List.map field_to_string field.fields) |> String.concat " ") ^ "}"
+    else "")
+
+let query_to_string ast =
+    (* being with root query *)
+    "{" ^ (ast.identifier) ^ "{" ^ 
+    ((List.map field_to_string ast.fields) |> String.concat " ")
+    ^ "}}"
--- test/test.ml
+++ test/test.ml
@@ -12,19 +12,19 @@ let test_parser_single_field test_ctxt =
   assert_equal 
     queryResult
     ( Ok { fields = [ { identifier = "jim"; fields = []; alias = None }]; 
-             identifier = "single"; arguments = [] } )
+           identifier = "single"; arguments = [] } )
 
 (* Test nested queries *)
 let test_parser_sub_fields test_ctxt =
   match Graphqaml.parse_query "{ nested { top { bottom } } }" with
-   | Ok _ -> ()
-   | Error msg -> failwith ("Failed to parse: " ^ msg)
+  | Ok _ -> ()
+  | Error msg -> failwith ("Failed to parse: " ^ msg)
 
 (* Test obvious bad grammar - non-matching parens *)
 let test_error_message test_ctxt =
   (match Graphqaml.parse_query "{ mismatch { top { bottom } }" with
-    | Error msg -> ()
-    | Ok _ -> failwith "Should not have received value")
+   | Error msg -> ()
+   | Ok _ -> failwith "Should not have received value")
 
 (* Test aliases *)
 let test_alias test_ctxt = 
@@ -34,19 +34,34 @@ let test_alias test_ctxt =
     ( Ok { arguments = [];
            identifier = "query";
            fields = 
-           [ { identifier = "jim"; 
-               fields = [];
-               alias = Some "name";                 
-           } ] } )
-    
+             [ { identifier = "jim"; 
+                 fields = [];
+                 alias = Some "name";                 
+               } ] } )
+
+
+(* Test serialisation (round trip) *)
+let test_serialisation test_ctxt =
+  let query_text = "{query{name age location {town country}}}" in
+  let query_ast = match Graphqaml.parse_query query_text with
+                  | Ok obj -> obj 
+                  | _ -> failwith "Bad query" in
+  let serialized_result = (Graphqaml.query_to_string query_ast) in
+  assert_equal ~msg: (Printf.sprintf "Expected %s, got %s" 
+                      query_text serialized_result) 
+               serialized_result query_text
 
 let parseTests =
-  "parser_tests" >:::
+  "graphqaml_tests" >:::
   [ "test_empty_query" >:: test_parser_empty_query;
     "test_single_field" >:: test_parser_single_field;
     "test_parser_sub_fields" >:: test_parser_sub_fields;
     "test_error_message" >:: test_error_message; 
-    "test_alias" >:: test_alias; ]
+    "test_alias" >:: test_alias; 
+
+    "test_serialisation" >:: test_serialisation;
+  ]
+
 
 let () =
   run_test_tt_main parseTests
